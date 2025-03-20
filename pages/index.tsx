408a33738e1b96dde1cdc4e664ceb5693a11ae55
seabee <3200104392@zju.edu.cn>
2025-03-20 16:16:10 +0800

    init

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home(props: any[]) {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1>Hello Future</h1>
+      </main>
+    </div>
+  );
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      pageName: "Home",
+    },
+  };
+}
