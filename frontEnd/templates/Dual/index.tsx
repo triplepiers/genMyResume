e4c73c2747a3815795fab287d492d2e2683457a3
seabee <3200104392@zju.edu.cn>
2025-04-21 01:08:32 +0800

    add: D04 (D01 镜像) bug: 同 D01

--- frontEnd/templates/Dual/index.tsx
+++ frontEnd/templates/Dual/index.tsx
@@ -1,11 +1,13 @@
 import D01 from './01'
 import D02 from './02'
 import D03 from './03'
+import D04 from './04'
 
 const Duals = {
     D01,
     D02,
-    D03
+    D03,
+    D04
 }
 
 export default Duals
